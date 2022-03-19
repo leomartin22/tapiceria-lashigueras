375b94471813c98ad16db9f4586e4f3c74805a77
Leomartin22 <leonardomartin.ldv@gmail.com>
2022-03-19 18:48:56 -0300

    estructura pagina contacto finalizada sin funcionalidad

--- assets/js/carrusel.js
+++ assets/js/carrusel.js
@@ -1,18 +1,6 @@
-$('section.awSlider .carousel').carousel({
-	pause: "hover",
-  interval: 2000
+$(".XXXXX__slider").slick({});
+
+$(".XXXXX__slider").slickLightbox({
+    itemSelector: "a",
+    navigateByKeyboard: true
 });
-
-var startImage = $('section.awSlider .item.active > img').attr('src');
-$('section.awSlider').append('<img src="' + startImage + '">');
-
-$('section.awSlider .carousel').on('slid.bs.carousel', function () {
- var bscn = $(this).find('.item.active > img').attr('src');
-	$('section.awSlider > img').attr('src',bscn);
-});
-
-
-/* 
-Philips ambilight tv
-Üzerine gleince duruyor slide
-*/
